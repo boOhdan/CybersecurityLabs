00c0a0fad9e8099415c3e87c909b3d8634f93bbb
XIAOIIII <boOhdan>
2021-10-28 20:02:14 +0300

    Created software attack a single-byte XOR and Vigenere ciphers

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -6,7 +6,25 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        }
+
+        static string Task1(string hexText) 
+        {
+            var asciiText = string.Empty;
+
+            for (int i = 0; i < hexText.Length; i += 2)
+            {
+                asciiText += (char)Convert.ToInt32(hexText.Substring(i, 2), 16);
+            }
+
+            return asciiText.DecodeXorCipher(((char)55).ToString());
+        }
+
+        static string Task2(string base64Text)
+        {
+            var asciiText = base64Text.DecodeFromBase64();
+
+            return asciiText.DecodeVigenereCipherWithoutKey();
         }
     }
 }
