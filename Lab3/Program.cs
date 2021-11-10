fcb849fb5d4476420725ed052d1d1c08a54a726b
XIAOIIII <boOhdan>
2021-11-10 08:37:51 +0200

    Created logic for cracking a MT19937 number generator

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -10,13 +10,40 @@ namespace Lab3
     {
         static void Main(string[] args)
         {
-            CrackLCG();
+        }
+
+        static void CrackMT19937()
+        {
+            var serverConnection = new ServerConnection();
+            var account = serverConnection.CreateAccountAsync("first63").Result;
+
+            uint time = (uint)DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+            var message = serverConnection.MakeBetAsync(account.AccountId, 1, 7777, "Mt").Result;
+            var mt19937 = new MT19937();
+
+            int i = 0;
+
+            while (true)
+            {
+                mt19937.seed_mt((uint)(time + i));
+
+                if (message.RealNumber == mt19937.extract_number())
+                    break;
+
+                i++;
+            }
+
+            var seed = time + i;
+            Console.WriteLine("Seed: " + seed);
+
+            var winMessage = serverConnection.MakeBetAsync(account.AccountId, 1, mt19937.extract_number(), "Mt").Result;
+            Console.WriteLine("Win Message: " + winMessage.Message);
         }
 
         public static void CrackLCG() 
         {
             var serverConnection = new ServerConnection();
-            var account = serverConnection.CreateAccountAsync("first21").Result;
+            var account = serverConnection.CreateAccountAsync("first22").Result;
             var Lcg = new Lcg() { Modulus = (int) Math.Pow(2, 32) };
 
             for(int i = 0; i < 50; i++) 
@@ -25,7 +52,7 @@ namespace Lab3
 
                 for (int j = 0; j < 3; j++)
                 {
-                    states[j] = serverConnection.MakeBetAsync(account.AccountId, 1, 7777, "Lcg").Result.RealNumber;
+                    //states[j] = serverConnection.MakeBetAsync(account.AccountId, 1, 7777, "Lcg").Result.RealNumber;
                 }
 
                 if (CrackingLcg.TryCrackUnknownMultiplier(states, Lcg.Modulus, out int multiplier))
---
+++ Lab3/RNGs/MT19937.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+
+namespace Lab3.RNGs
+{
+    public class MT19937
+    {
+        public int w = 32, n = 624;
+        public uint f = 1812433253;
+        public int m = 397, r = 31;
+        public uint a = 0x9908B0DF;
+        public uint d = 0xFFFFFFFF, b = 0x9D2C5680, c = 0xEFC60000;
+        public int u = 11, s = 7, t = 15, l = 18;
+
+        public uint[] MT;
+        public int index;
+
+        public void seed_mt(uint seed)
+        {
+            index = n;
+            MT = new uint[n];
+            MT[0] = seed;
+
+            for (int i = 1; i < MT.Length; i++)
+            {
+                MT[i] = (uint)(f * (MT[i - 1] ^ (MT[i - 1] >> (w - 2))) + i);
+            }
+        }
+
+        public uint extract_number()
+        {
+            if (index == n)
+                twist();
+
+            uint y = MT[index];
+
+            y ^= (y >> u) & d;
+            y ^= (y << s) & b;
+            y ^= (y << t) & c;
+            y ^= y >> l;
+
+            index++;
+            return y;
+        }
+
+        public void twist()
+        {
+            for (int i = 0; i < MT.Length; i++)
+            {
+                int lower_mask = (1 << r) - 1;
+                int upper_mask = ~lower_mask;
+                uint tmp = (uint)((MT[i] & upper_mask) + (MT[(i + 1) % n] & lower_mask));
+                uint tmpA = tmp >> 1;
+
+                if (tmp % 2 != 0)
+                {
+                    tmpA ^= a;
+                }
+
+                MT[i] = MT[(i + m) % n] ^ tmpA;
+            }
+
+            index = 0;
+        }
+    }
+}
