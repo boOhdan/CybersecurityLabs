47a711a5541f93dcd94b3485578bf7a3f5b82f50
zhenya.musievsky <zhenya.musievsky@gmail.com>
2021-12-09 01:53:04 +0200

    symbol frequencies counts correctly

--- src/main/java/lab1/Utils.java
+++ src/main/java/lab1/Utils.java
@@ -2,25 +2,25 @@ package lab1;
 
 import org.apache.commons.lang3.StringUtils;
 
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
 
 public class Utils {
 
 	public static void countSymbolsFrequency(String text, PrintWriter writer) {
-		Map<Character, Integer> letterFrequencies = new TreeMap<>();
+		Map<Character, Integer> letterFrequencies = new HashMap<>();
 		char[] chars = text.toCharArray();
 		for (int i = 0; i < text.toCharArray().length; i++) {
 			letterFrequencies.compute(chars[i], (k, v) -> v != null ? v + 1 : 1);
 		}
 
-
-		Set<Map.Entry<Character, Integer>> entrySet = new TreeSet<>((o1, o2) -> o2.getValue().compareTo(o1.getValue()));
-		entrySet.addAll(letterFrequencies.entrySet());
-
-		entrySet.forEach(e -> writer.println(e.getKey() + " = " + e.getValue()));
+		List<Map.Entry<Character, Integer>> sortedList = new ArrayList<>(letterFrequencies.entrySet());
+		sortedList.sort((e1, e2) -> {
+			int value = e1.getValue().compareTo(e2.getValue());
+			return value != 0 ? value : e1.getKey().compareTo(e2.getKey());
+		});
+		Collections.reverse(sortedList);
+		sortedList.forEach(e -> writer.println(e.getKey() + " = " + e.getValue()));
 	}
 
 	public static void countIndexesOfCoincidence(String text, PrintWriter writer) {
