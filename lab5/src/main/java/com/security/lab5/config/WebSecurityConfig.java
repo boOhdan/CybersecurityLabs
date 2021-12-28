d6b8d3a0d6c3febf642ee5ddfcc207b85be21ff7
zhenya.musievsky <zhenya.musievsky@gmail.com>
2021-12-28 15:05:11 +0200

    hash algorithm changed to bcrypt

--- lab5/src/main/java/com/security/lab5/config/WebSecurityConfig.java
+++ lab5/src/main/java/com/security/lab5/config/WebSecurityConfig.java
@@ -10,8 +10,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -58,7 +58,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return new SCryptPasswordEncoder();
+		return new BCryptPasswordEncoder();
 	}
 
 	@Override
