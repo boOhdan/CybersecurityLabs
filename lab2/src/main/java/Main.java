27f5b8857529922ee31b791b293e4effdcf57a32
zhenya.musievsky <zhenya.musievsky@gmail.com>
2022-01-17 01:43:53 +0200

    found To gru combination

--- lab2/src/main/java/Main.java
+++ lab2/src/main/java/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
 	private static final Pattern wordPattern = Pattern.compile("[\\p{Alpha} ]+");
 
-	private static final String WORD = "And t";
+	private static final String WORD = "To gru";
 	private static final String DESTINATION = "src/main/resources/decrypted.txt";
 
 	private static final double MATCH_PERCENTAGE = 0.7;
