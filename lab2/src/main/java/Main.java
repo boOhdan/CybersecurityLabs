7862e321f11f3444d84724d5cd9d86b2e8958ad6
zhenya.musievsky <zhenya.musievsky@gmail.com>
2022-01-17 01:54:46 +0200

    "And lose "

--- lab2/src/main/java/Main.java
+++ lab2/src/main/java/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
 	private static final Pattern wordPattern = Pattern.compile("[\\p{Alpha} ]+");
 
-	private static final String WORD = "The undisputed";
+	private static final String WORD = "And lose ";
 	private static final String DESTINATION = "src/main/resources/decrypted.txt";
 
 	private static final double MATCH_PERCENTAGE = 0.7;
